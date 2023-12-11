a2dc6c59d1cdd88ccfe707eb8148d7a65bb1048a
Vasil Pavlov <vasil@vasilpavlov.com>
2023-12-11 20:21:43 +0200

    Add event listeners, DOM manipulation

--- userCard.js
+++ userCard.js
@@ -30,8 +30,8 @@ template.innerHTML = `
         <div>
           <h3></h3>
         <div class="info">
-          <p>EMAIL</p>
-          <p>PHONE</p>
+          <p><slot name="email"></slot></p>
+          <p><slot name="phone"></slot></p>
         </div>
         <button id="toggle-info">Hide Info</button>
         </div>
@@ -41,10 +41,31 @@ template.innerHTML = `
 class UserCard extends HTMLElement {
     constructor() {
         super();
+        this.showInfo = true;
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+    }
+
+    toggleInfo() {
+      this.showInfo = !this.showInfo;
+      const info = this.shadowRoot.querySelector('.info');
+      const toggleBtn = this.shadowRoot.querySelector('#toggle-info');
+      if(this.showInfo) {
+        info.style.display = 'block';
+        toggleBtn.innerText = "Hide Info";
+      } else {
+        info.style.display = 'none';
+        toggleBtn.innerText = "Show Info";
+      }
+    }
+
+    connectedCallback() {
+      this.shadowRoot.querySelector('#toggle-info').addEventListener('click', () => this.toggleInfo());
+    }
+    disconnectedCallback() {
+      this.shadowRoot.querySelector('#toggle-info').removeEventListener();
     }
 }
 
